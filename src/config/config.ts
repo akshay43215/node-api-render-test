17d3cde853cec3cc88ae0d81609386d0ce7743d1
akshay43215 <akshaykrishna24996@gmail.com>
2025-07-14 00:25:23 +0530

    commit by with env configs

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,7 +1,6 @@
 import {config as conf} from 'dotenv';
 conf();
 
-
 const _config ={
     port: process.env.PORT,
     databaseUrl: process.env.MONGO_CONNECTION_STRING,
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,14 @@
 import express from 'express'
 import { globalErrorHandler } from './middleware/globalErrorHandler.js';
 import { userRouter } from './user/userRoute.js';
+import { connectToDatabase } from './config/db.js';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
- app.use('/api/users', userRouter);
+app.use('/api/users', userRouter);
+await connectToDatabase();
 
 app.get('/', (_req:any, res:any) => {
   res.send('Hello ADDED CHANGES from TypeScript + Node.js on Render!');
